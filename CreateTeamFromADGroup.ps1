c372a7e372fd505c245c190dc1111a9fb2967516
Joshua Froelich <josh@shwaa.co.uk>
2020-05-26 19:17:22 +0100

    Added More Code and Comments
    Changed:
    Moved OU to a variable for easier editing
    Updated Comments
    
    Added:
    Code for the creation of the Group and conversion to Team

--- CreateTeamFromADGroup.ps1
+++ CreateTeamFromADGroup.ps1
@@ -6,7 +6,6 @@
 # For each group:
 #   If Group does not exist in O365
 #       Create group in O365
-#       Add Salamander 365 service principal as owner (e43dbdcd-37c1-4d43-a4d2-36a00903e568)
 #       Set O365 group to be hidden from address list
 #       Turn off welcome email
 #       Add members from AD group as members
@@ -18,6 +17,11 @@
 # Sort CSV alphabetically
 # Write out CSV
 
+# Variables
+
+$ADGroupsOU = "OU=Teams Groups,OU=Groups,OU=_Bedales,DC=bedales,DC=org,DC=uk"
+
+
 #  Load Modules
 Import-Module ActiveDirectory
 Import-Module -UseWindowsPowerShell AzureAD
@@ -27,19 +31,48 @@ Import-Module -UseWindowsPowerShell AzureAD
 # Import CSV
 $CSV = import-csv .\TeamsGroupSync.csv
 
-$ADGroups = Get-ADGroup -SearchBase "OU=Teams Groups,OU=Groups,OU=_Bedales,DC=bedales,DC=org,DC=uk" -filter {GroupCategory -eq "Security"}
+$ADGroups = Get-ADGroup -SearchBase $ADGroupsOU -filter {GroupCategory -eq "Security"}
 
 foreach ($ADGroup in $ADGroups) {
     # Extract the O365 group name from the AD group name
     $O365GroupName = $ADGroup.Name -replace "Teams " 
-    #Write-Output $O365GroupName
     $O365GroupExists = Get-AzureADGroup -SearchString $O365GroupName
-
+    
+    # Check if group exists in Azure
     if ($O365GroupExists -eq $null){
-        Write-Output "Nothing Here"
+        # Group does not exist in Azure
+        Write-Output "Creating Office 365 Group"
+        
+        # Create O365 Group
+        CODE TO CREATE GROUP
+        
+        $O365Group = Get-AzureADGroup -SearchString $O365GroupName
+        
+        # Disable Welcome Message
+        
+        
+        # Hide from GAL
+        
+        
+        # Set description to be same as AD group
+        
+        
+        # Add members of AD group as owners
+        
+        
+        # Add members of AD group as members
+        
+        
+        # Create Basic locked down Team from O365 Group
+        new-team -GroupID $O365Group.ObjectID  -AllowGiphy $false -AllowStickersAndMemes $false -AllowCustomMemes $false -AllowGuestCreateUpdateChannels $false -AllowGuestDeleteChannels $false -AllowCreateUpdateChannels $false -AllowDeleteChannels $false -AllowAddRemoveApps $false -AllowCreateUpdateRemoveTabs $false -AllowCreateUpdateRemoveConnectors $false -ShowInTeamsSearchAndSuggestions $false -AllowOwnerDeleteMessages $true 
+        
+        # Add new AD group/O365 group pair to CSV
+        $newRow = New-Object PSObject -Property @{ ADGroup = $ADGroup.Name ; O365Group = $O365GroupName }
+        $CSV += $newRow
+        
     }else {
-        Write-Output "Here is the group:"
-        Write-Output $O365GroupExists.DisplayName
+        # Group exists in Azure
+        Write-Output "Group $O365GroupExists already exists in AzureAD"
     }
 
 
